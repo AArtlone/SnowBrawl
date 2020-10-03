f633592809b73622001829c9c121f642d039fe14
Artem <artem.stolyga@gmail.com>
2020-10-03 22:42:17 +0200

    Created a tab system that works with images and colors

---
+++ SnowBrawl/Assets/Scripts/NewScripts/TabSystem/TabButton.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    [SerializeField] private TabGroup tabGroup;
+
+    public Image BackgroundImage { get; private set; }
+
+    private void Awake()
+    {
+        BackgroundImage = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.EnterTab(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.SelectTab(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.ExitTab();
+    }
+
+    public void Select(Color color)
+    {
+        UpdateVisual(color);
+    }
+
+    public void Select(Sprite sprite)
+    {
+        UpdateVisual(sprite);
+    }
+
+    public void Deselect()
+    {
+
+    }
+
+    public void UpdateVisual(Color color)
+    {
+        Color newColor = new Color(color.r, color.g, color.b, 1f);
+        BackgroundImage.color = newColor;
+    }
+
+    public void UpdateVisual(Sprite sprite)
+    {
+        BackgroundImage.sprite = sprite;
+    }
+}
---
+++ SnowBrawl/Assets/Scripts/NewScripts/TabSystem/TabGroup.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    [SerializeField] private bool useColor;
+
+    [SerializeField] private Sprite idleSprite;
+    [SerializeField] private Sprite hoverSprite;
+    [SerializeField] private Sprite activeSprite;
+
+    [SerializeField] private Color idleColor;
+    [SerializeField] private Color hoverColor;
+    [SerializeField] private Color activeColor;
+
+    private List<TabButton> tabButtons;
+
+    [SerializeField] private List<GameObject> pages;
+
+    private TabButton selectedTab;
+
+    private void Start()
+    {
+        ResetTabButtonsVisuals();
+
+        ResetPages();
+
+        int index = tabButtons[0].transform.GetSiblingIndex();
+
+        SelectTab(tabButtons[index]);
+    }
+
+    public void Subscribe(TabButton tabButton)
+    {
+        if (tabButtons == null)
+            tabButtons = new List<TabButton>();
+
+        tabButtons.Add(tabButton);
+    }
+
+    public void EnterTab(TabButton tabButton)
+    {
+        if (selectedTab == null || tabButton == selectedTab)
+            return;
+
+        if (useColor)
+            tabButton.UpdateVisual(hoverColor);
+        else
+            tabButton.UpdateVisual(hoverSprite);
+    }
+
+    public void SelectTab(TabButton tabButton)
+    {
+        if (selectedTab != null)
+            selectedTab.Deselect();
+
+        selectedTab = tabButton;
+
+        ResetTabButtonsVisuals();
+
+        if (useColor)
+            selectedTab.Select(activeColor);
+        else
+            selectedTab.Select(activeSprite);
+
+        int index = tabButton.transform.GetSiblingIndex();
+
+        for (int i = 0; i < pages.Count; i++)
+            pages[i].SetActive(index == i);
+    }
+
+    public void ExitTab()
+    {
+        ResetTabButtonsVisuals();
+    }
+
+    private void ResetTabButtonsVisuals()
+    {
+        foreach (TabButton button in tabButtons)
+        {
+            // We dont reset the selected tab button
+            if (selectedTab != null && selectedTab == button)
+                continue;
+
+            if (useColor)
+            {
+                Color color = new Color(idleColor.r, idleColor.g, idleColor.b, 1f);
+
+                button.UpdateVisual(color);
+
+                continue;
+            }
+
+            button.UpdateVisual(idleSprite);
+        }
+    }
+
+    private void ResetPages()
+    {
+        pages.ForEach(p => p.SetActive(false));
+    }
+}
